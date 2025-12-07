5d3cb9676b563533c28dd4b2cc6966382e949a3c
Victor Okou <victor.okou@efrei.net>
2025-12-07 16:03:46 +0100

    utils functions

--- .idea/hogwart/utils/input_utils.py
+++ .idea/hogwart/utils/input_utils.py
@@ -5,28 +5,27 @@ def ask_text(question):
 
     return answer
 
-import json
 
-with open("inventory.json","r") as f:
-    inventory = json.load(f)
+def ask_choice(question,choices_list):
+    print(question)
+    size_list = len(choices_list)
+    values_and_index=[]
+    for i in range(size_list):
+        print(f"{i+1}. {choices_list[i]}")
+
+        values_and_index.append((choices_list[i].lower()))
+        values_and_index.append(str(i+1).lower())
 
 
-for items_id, (items_name,price) in inventory.items():
-    nb_item = items_id
-    print(items_id," : ",items_name)
+    print("Your choice:", end=" ")
+    answer = input()
 
-item_chosed = []
-choice = input("Quel objet voulez vous choisir : ").split(" ")
-for i in choice :
-    if i in inventory:
-        item_name = inventory[i][0]
-        item_chosed.append(item_name)
-    else :
-        print("L'objet",i," n'est pas disponible bahahaa")
 
-print("Vous avez choisi : ",end=" ")
-for j in item_chosed:
-    print(j,end=", ")
+    while answer.lower() not in values_and_index:
+        print("Your choice have to be in the list :", end=" ")
+        answer = (input())
+
+    return answer
 
 
 #print(F"name : {inventory('name')}")
