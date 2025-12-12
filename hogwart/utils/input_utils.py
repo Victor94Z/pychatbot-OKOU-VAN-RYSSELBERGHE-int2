aa907e7c27916d9b60908bf573dc9511648c8e5f
Victor Okou <victor.okou@efrei.net>
2025-12-12 22:35:16 +0100

    Fix ask_number

--- hogwart/utils/input_utils.py
+++ hogwart/utils/input_utils.py
@@ -35,8 +35,80 @@ def load_file(address):
         file = json.load(f)
         return file
 
-def ask_number(a,b):
-    choice = -1
-    while choice < a or choice > b:
-        choice = int(input(f"Choose a number between {a} and {b} : "))
-    return choice 
+def validate_number(answer, min_val=None, max_val=None):
+    is_valid_number = True
+
+    # Check answer not empty
+    if len(answer) == 0 or answer.isspace():
+        is_valid_number = False
+        return is_valid_number,answer
+
+    # Check if the answer is negative
+    is_negative = False
+    abs_index = 0
+
+    if answer[0] == '-':
+        is_negative = True
+        abs_index = 1
+        if len(answer) == 1:
+            is_valid_number = False
+            return is_valid_number,answer
+
+    # Check if each char is a valid digit
+    for char in answer[abs_index:]:
+        if char < '0' or char > '9':
+            is_valid_number = False
+            return is_valid_number,answer
+
+
+    # Convert answer to int
+    converted_answer = 0
+    for c in answer[abs_index:]:
+        digit = ord(c) - ord('0')
+        converted_answer = converted_answer * 10 + digit
+
+    if is_negative:
+        converted_answer = -converted_answer
+
+    # Check range if provided
+    if min_val is not None:
+        if converted_answer < min_val:
+            is_valid_number = False
+            return is_valid_number,converted_answer
+    if max_val is not None:
+        if converted_answer > max_val:
+            is_valid_number = False
+            return is_valid_number,converted_answer
+
+    return is_valid_number,converted_answer
+
+def ask_number(message, min_val=None, max_val=None):
+
+    #Swap bounds if not valid
+    if min_val is not None and max_val is not None:
+        if min_val > max_val:
+            min_val,max_val = max_val,min_val
+
+    answer=input(message).strip()
+
+    # Determine error message
+    error_message = "Please enter a number"
+    if min_val is not None and max_val is not None:
+        error_message += " between " + str(min_val) + " and " + str(max_val)
+    elif min_val is not None and max_val is None:
+        error_message += " higher than " + str(min_val)
+    elif min_val is None and max_val is not None:
+        error_message += " lower than " + str(max_val)
+
+    error_message += " : "
+
+    # Number validation
+    is_valid_number, converted_answer = validate_number(answer, min_val, max_val)
+    while not is_valid_number:
+        answer = input(error_message).strip()
+        is_valid_number, converted_answer = validate_number(answer, min_val, max_val)
+
+
+    return converted_answer
+
+
