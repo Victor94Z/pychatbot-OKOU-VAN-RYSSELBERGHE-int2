f7ccd42ae64883534e642bdc48a6c00b58c22f94
Victor Okou <victor.okou@efrei.net>
2025-12-14 11:37:22 +0100

    Add the fonction receive_letter()

--- hogwart/chapters/chapter_1.py
+++ hogwart/chapters/chapter_1.py
@@ -15,12 +15,12 @@ def introduction():
     print()
     print(input("Press Enter to continue..."))
     print('{:^130}'.format("Today marks the beginning of a new chapter in your life."))
-    time.sleep(3)
+    time.sleep(2)
     print('{:^130}'.format("You stand on the edge of something unknown, something extraordinary, even if you do not fully understand it yet."))
     print()
     print(input("Press Enter to continue..."))
     print('{:^130}'.format("An incredible journey awaits you — a journey filled with discoveries, challenges, and wonders beyond imagination."))
-    time.sleep(4)
+    time.sleep(3)
     print('{:^130}'.format("Your story begins now."))
     print()
     print(input("Press Enter to continue..."))
@@ -100,10 +100,54 @@ def  create_character():
     print("Attributes:")
     for attr, value in character["Attributes"].items():
         print(f"- {attr}: {value}")
+    print()
+    print(input("Press Enter to continue..."))
 
 
 
 
 
-create_character()
+def receive_letter():
+    print()
+
+    print(('{:^130}'.format("Night falls quietly around you.")))
+    time.sleep(1.3)
+    print('{:^130}'.format("The world seems calm… almost too calm."))
+    time.sleep(2)
+    print('{:^130}'.format("Suddenly, the sound of wings cuts through the silence."))
+    print()
+    print(input("Press Enter to continue..."))
+    print('{:^130}'.format("The creature observes you for a moment before extending its leg."))
+    time.sleep(2)
+    print(('{:^130}'.format("Attached to it is an old parchment, carefully rolled and sealed with red wax bearing the Hogwarts crest.")))
+    print()
+    print(input("Press Enter to continue..."))
+    print(('{:^130}'.format("Moments later, the owl takes flight and disappears into the darkness, leaving the letter behind.")))
+    time.sleep(2)
+    print(('{:^130}'.format("Curious and slightly nervous, you pick it up and gently break the seal.")))
+    print()
+    print(input("Press Enter to read the letter..."))
+    print()
+    print(('{:^130}'.format("---------------------------------------------------------------------------")))
+    print()
+    print("Dear Student,")
+    print()
+    time.sleep(1.2)
+    print("We are pleased to inform you that you have been accepted to Hogwarts School of Witchcraft and Wizardry!” ")
+    print()
+    time.sleep(1.2)
+    accept_invitation=ask_choice("Do you accept this invitation and go to Hogwarts?", ["Yes, of course! ", "No, I'd rather stay with Uncle Vernon..."])
+    if accept_invitation.lower() == "yes, of course!" or accept_invitation.lower() == "1":
+        print()
+        print("Congratulations,you have been accepted to Hogwarts!")
+    else:
+        print()
+        print("You tear up the letter, and Uncle Vernon cheers :","\n","\n","EXCELLENT! Finally, someone NORMAL in this house!")
+        print()
+        print("The magical world will never know you existed...")
+        print()
+        time.sleep(1.2)
+        print(('{:^130}'.format("GAME OVER")))
+        exit()
+
 
