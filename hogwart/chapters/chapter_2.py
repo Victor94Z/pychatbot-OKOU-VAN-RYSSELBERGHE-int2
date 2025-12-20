5e237b5e62084ea26f2b9ea87c9f90aafb9604f9
Adhenis <adhenis@icloud.com>
2025-12-20 16:13:56 +0100

    slow_print

--- hogwart/chapters/chapter_2.py
+++ hogwart/chapters/chapter_2.py
@@ -1,101 +1,115 @@
-from chapter_1 import create_character
-character = create_character("prenom","nom",["Courage", "Intelligence", "Loyalty", "Ambition"])
+import time
+from hogwart.chapters.chapter_1 import create_character
+from hogwart.utils.input_utils import slow_print
+player = create_character()
 
 def meet_friend(character):
 
     changed_attributes = []
 
-    print("You board the Hogwarts Express. The train slowly departs northward...")
-    print("A red-haired boy enters your compartment, looking friendly.")
+    slow_print('{:^130}'.format("You board the Hogwarts Express. The train slowly departs northward..."))#train introduction
+    time.sleep(2)
 
-    answer_ron = {"1" : "Sure, have a seat !","2" : "Sorry, i prefer to travel alone."}
-    print("—Hi! I'm Ron Weasley. Mind if I sit with you?" "\n"
-          "How do you respond ? : ")
-    for i in answer_ron:
-        print(i)
+
+    slow_print('{:^130}'.format("A red-haired boy enters your compartment, looking friendly."))
+    time.sleep(2)
+
+    answer_ron = {"1" : "Sure, have a seat !","2" : "Sorry, i prefer to travel alone."}#ron possible responses
+    slow_print("—Hi! I'm Ron Weasley. Mind if I sit with you ? ")
+    time.sleep(1)
+    slow_print("How do you respond ? : \n")
+
+    for i in answer_ron:#print the resposnses
+        slow_print(f'{i}: {answer_ron[i]}')
     answer = 0
-    while answer < 1 or answer > len(answer_ron):
-        answer = input("Your choice : ")
+    while answer < 1 or answer > len(answer_ron): #verify if the responses chosed by the player is valid
+        answer = int(input("\n Your choice : "))
         if answer < 1 or answer > len(answer_ron): 
-            print("This is not  valid choice")
-    print("Your choice : ",answer)
+            slow_print("This is not  valid choice")
     if answer == 1 :
-        print("Ron smiles: — Awesome! You'll see, Hogwarts is amazing!")
+        slow_print("\n Ron smiles: — Awesome! You'll see, Hogwarts is amazing! \n")
         change = "Loyalty"
-        character[change] += 1
-        if not change in changed_attributes:
+        character["Attributes"][change] += 1
+        if not change in changed_attributes:#change the attributes from the response
             changed_attributes.append(change)
     elif answer == 2 :
-        print("Ron break down in tears: - NOOOOOOOOO WHHYYYYYYYYYY !!!!!!!","\n",
-              "...""\n",
-              "he evaporates in the air as you think of what you did")
+        slow_print("\n Ron break down in tears: - NOOOOOOOOO WHHYYYYYYYYYY !!!!!!! ")
+        time.sleep(1)
+        slow_print("...")
+        time.sleep(1)
+        slow_print("he evaporates in the air as you think of what you did...\n")
         change = "Ambition"
-        character[change] += 1
+        character["Attributes"][change] += 1
         if not change in changed_attributes:
             changed_attributes.append(change)
 
-    print("A girl enters next, already carrying a stack of books.")
+    slow_print('{:^130}'.format("A girl enters next, already carrying a stack of books."))
+    time.sleep(2)
 
-    answer_hermione = {"1" : "Yes, I love learning new things!","2" : "Uh… no, I prefer adventures over books."}
-    print("— Hello, I'm Hermione Granger. Have you ever read 'A History of Magic'?""\n"
-            "How do you respond? : ")
+    answer_hermione = {"1" : "Yes, I love learning new things!","2" : "Uh… no, I prefer adventures over books."}#hermione possible resposnses
+    slow_print("— Hello, I'm Hermione Granger. Have you ever read 'A History of Magic' ? \n"
+            "\n"
+            "How do you respond ? : \n")
     for i in answer_hermione:
-        print(i)
+        slow_print(i,": ",answer_hermione[i])
     answer = 0
-    while answer < 1 or answer > len(answer_hermione):
-        answer = input("Your choice : ")
+    while answer < 1 or answer > len(answer_hermione):#verify like with ron
+        answer = int(input("\n Your choice : "))
         if answer < 1 or answer > len(answer_hermione): 
-            print("This is not  valid choice")
-    print("Your choice : ",answer)
+            slow_print("This is not  valid choice")
     if answer == 1 :
-        print("Hermione smiles, impressed: — Oh, that's rare! You must be very clever!")
+        slow_print("Hermione smiles, impressed: — Oh, that's rare! You must be very clever ! \n")
         change = "Intelligence"
-        character[change] += 1
+        character["Attributes"][change] += 1
         if not change in changed_attributes:
             changed_attributes.append(change)
     elif answer == 2 :
-        print("Hermione looks at you in a disappointed way: -I thought u would have been different from the others...\n" \
-            "You're finally not special at all.")
+        slow_print("Hermione looks at you in a disappointed way: -I thought you would have been different from the others...\n",time.sleep(2),
+            "You're finally not special at all. \n")
         #insert video de moi qui cracvhe sur victor
         change = "Courage"
-        character[change] += 1
+        character["Attributes"][change] += 1
         if not change in changed_attributes:
             changed_attributes.append(change)
+    
+    slow_print('{:^130}'.format("\n Then a blonde boy enters, looking arrogant.\n"))
+    time.sleep(2)
 
-    print("Then a blonde boy enters, looking arrogant.")
-
-    answer_draco = {"1" : "Shake his hand politely.","2" : "Ignore him completely.","3" : "Respond with arrogance."}
-    print("— I'm Draco Malfoy. It's best to choose your friends carefully from the start, don't you think?""\n"
-          "How do you respond ? : ")
+    answer_draco = {"1" : "Shake his hand politely.","2" : "Ignore him completely.","3" : "Respond with arrogance."}#draco possible responses
+    slow_print("— I'm Draco Malfoy. It's best to choose your friends carefully from the start, don't you think ? \n"
+          "\n"
+          "How do you respond ? : \n")
     for i in answer_draco:
-        print(i)
+        slow_print(i," :",answer_draco[i])
     answer = 0
     while answer < 1 or answer > len(answer_draco):
-        answer = input("Your choice : ")
+        answer = int(input("\n Your choice : "))
         if answer < 1 or answer > len(answer_draco): 
-            print("This is not  valid choice")
-    print("Your choice : ",answer)
+            slow_print("This is not  valid choice")
     if answer == 1 :
-        print("Draco is trying not to laugh: -Good boy.")
+        slow_print("Draco is trying not to laugh: -Good boy.")
         change = "Ambition"
-        character[change] += 1
+        character["Attributes"][change] += 1
         if not change in changed_attributes:
             changed_attributes.append(change)
     elif answer == 2 :
-        print("Draco frowns, annoyed. — You'll regret that!")
+        slow_print("Draco frowns, annoyed. — You'll regret that!")
         change = "Loyalty"
-        character[change] += 1
+        character["Attributes"][change] += 1
         if not change in changed_attributes:
             changed_attributes.append(change)
     elif answer == 3 :
-        print("Draco does a backflip and brake the glasse behind him, as he's flying out of the train he's cursing you:\n -YYOoOOuUUuuU MmAAaaADddDEeeE TTtHHHhIIiSssS HHhAAapPPEeeEnNNDD !!!!")
+        slow_print("Draco does a backflip and brake the glasse behind him, as he's flying out of the train he's cursing you:\n -YYOoOOuUUuuU MmAAaaADddDEeeE TTtHHHhIIiSssS HHhAAapPPEeeEnNNDD !!!! \n ...")
         change = "Courage"
-        character[change] += 1
+        character["Attributes"][change] += 1
         if not change in changed_attributes:
             changed_attributes.append(change)
 
-    print("The train continues its journey. Hogwarts Castle appears on the horizon...")
-    print("Your choices already say a lot about your personality!""\n"
+    slow_print('{:^130}'.format("The train continues its journey. Hogwarts Castle appears on the horizon...\n"))
+    slow_print("Your choices already say a lot about your personality!""\n"
             "Your updated attributes:",end=" ")
     for i in changed_attributes:
-        print(change,character[i],end=" ")
+        slow_print(f'{i} : {character["Attributes"][i]}',end=", ")
+
+
+meet_friend(player)
--- hogwart/utils/input_utils.py
+++ hogwart/utils/input_utils.py
@@ -1,4 +1,12 @@
 import json
+import time 
+
+
+def slow_print(text, delay=0.03):
+    for char in text:
+        print(char, end="", flush=True)
+        time.sleep(delay)
+    print()
 
 def ask_text(question):
 
